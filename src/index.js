cf4deaf0ba6cddc7037b600121846c0ac7ab2768
Carmen Liu <carmen.liujia@gmail.com>
2018-02-07 10:50:17 +1300

    make the basic React works

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Create a new component should produce some HTML
+const App = () => {
+    return <div>Hi</div>;
+}
+// Take the component's generated HTML and put it on the page(in the DOM)
+
+// React.render(App);
+// ReactDOM.render(React.createElement(App));
+ReactDOM.render(<App />,document.querySelector('.container') );
