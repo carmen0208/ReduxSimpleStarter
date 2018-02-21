1da91e81534fc52311d08a1f83f1f154edc84996
Carmen Liu <carmen.liujia@gmail.com>
2018-02-22 09:41:22 +1300

    add search bar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const API_KEY = 'you_youtube_api_key';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyBa9OZcD0kP89dVulxCmI28Xrsc-RR4i2Y';
 
 // Create a new component should produce some HTML
 const App = () => {
-    return <div>Hi</div>;
+    return (<div>
+        <SearchBar />
+    </div>);
 }
 // Take the component's generated HTML and put it on the page(in the DOM)
 
 // React.render(App);
 // ReactDOM.render(React.createElement(App));
-ReactDOM.render(<App />,document.querySelector('.container') );
+ReactDOM.render(<App />, document.querySelector('.container') );
