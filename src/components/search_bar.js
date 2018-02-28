9c9a6e02637f52eff5e65f4a759edde2d2348587
Carmen Liu <carmen.liujia@gmail.com>
2018-02-28 23:37:28 +1300

    state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,8 +8,21 @@ import React, {Component } from 'react';
 
 //class component
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { term: "Starting Value" };
+    }
     render() {
-        return <input onChange={(event) =>  console.log(event.target.value)}/>;
+        // return <input onChange={(event) =>  console.log(event.target.value)}/>;
+        //return <input onChange={(event) =>  this.setState({ term: event.target.value} )}/>
+        return (
+        <div>
+            <input value={this.state.term}
+            onChange={(event) =>  this.setState({ term: event.target.value} )}/>
+            Value of thi input: {this.state.term }
+        </div>
+        );
     }
     // inputText(event) {
     //     console.log(event.target.value);
