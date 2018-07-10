86382f28f01a205edb7d0fcf66b655578f8d9349
Carmen Liu <carmen.liujia@gmail.com>
2018-07-10 12:18:36 +1200

    dom-diff for all structure setting

---
+++ src/c5/DomDiff.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class RendorTree extends React.Component {
+  constructor (props) {
+    super(props)
+    this.wraper = this.wraper.bind(this)
+  }
+
+  wraper (event) {
+    console.log(event.target.id)
+    ReactDOM.render(<p >{event.target.id}</p>,
+      document.getElementById('diff-root'),
+      function () {
+        console.log('=====================')
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <button id='shape1' onClick={this.wraper}>Shape 1</button>
+        <button id='shape2' onClick={this.wraper}>Shape 2</button>
+        <button id='shape3' onClick={this.wraper}>Shape 3</button>
+        <button id='shape4' onClick={this.wraper}>Shape 4</button>
+        <button id='shape5' onClick={this.wraper}>Shape 5</button>
+        <button id='shape6' onClick={this.wraper}>Shape 6</button>
+        <button id='none' onClick={this.wraper}>Clear</button>
+        <div id='diff-root' />
+      </div>
+    )
+  }
+}
+
+export default RendorTree
